0b4ffcb2668c3d0127c31c111ff14560e21b3a87
Ian Adams <ijadams503@gmail.com>
2020-08-08 19:34:35 -0500

    ipad iframe

--- src/containers/Homepage/index.js
+++ src/containers/Homepage/index.js
@@ -17,10 +17,29 @@ const Homepage = () => {
                                 <h1 className="uk-text-lead">{homepage.herotext}</h1>
                                 <p>{homepage.subherotext}</p>
                                 <div className="button--container">
-                                    <a href=""><button className="uk-button uk-button-default one">{homepage.cta_button_1}</button></a>
-                                    <a href=""><button className="uk-button uk-button-default two">{homepage.cta_button_2}</button></a>
+                                    <a href="">
+                                        <button
+                                            className="uk-button uk-button-default one">{homepage.cta_button_1}</button>
+                                    </a>
+                                    <a href="">
+                                        <button
+                                            className="uk-button uk-button-default two">{homepage.cta_button_2}</button>
+                                    </a>
                                 </div>
                             </div>
+                        </section>
+                        <section className="hero--sub left">
+                            <div className="half ipad uk-cover-container">
+                                <iframe src="https://omniplayer.myomni.live/?k=demo4&amp;v=Tedx~TEDx_1_1.mp4" allowFullScreen="allowfullscreen"></iframe>
+                            </div>
+                            <div className="half">
+                                <h1>test</h1>
+                            </div>
+                        </section>
+                        <section className="hero--sub right">
+                        </section>
+                        <section className="hero--sub left">
+
                         </section>
                     </div>
                 );
