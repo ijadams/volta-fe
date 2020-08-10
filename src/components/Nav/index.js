c16c8730e52e0c9740fcf376946f82bb4c1eba70
Ian Adams <ijadams503@gmail.com>
2020-08-09 19:48:59 -0500

    fix logo

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -52,26 +52,18 @@ export class Nav extends Component {
             <div>
                 <Query query={NAV_QUERY} id={null}>
                     {({data: {nav, categories}}) => {
-                        const desktopimageUrlImageUrl =
-                            process.env.NODE_ENV !== "development"
-                                ? nav.navlogo.url
-                                : process.env.REACT_APP_BACKEND_URL + nav.navlogo.url;
-                        const mobileImageUrl =
-                            process.env.NODE_ENV !== "development"
-                                ? nav.navlogo.url
-                                : process.env.REACT_APP_BACKEND_URL + nav.navlogomobile.url;
-
+                        const desktopimageUrlImageUrl = process.env.REACT_APP_BACKEND_URL + nav.navlogo.url;
+                        const mobileImageUrl = process.env.REACT_APP_BACKEND_URL + nav.navlogomobile.url
                         return (
                             <div>
                                 <nav className={`uk-navbar-container ${this.state.scrolled ? "active" : ''}`} data-uk-navbar>
                                     <div className="uk-navbar-left">
                                         <ul className="uk-navbar-nav">
                                             <li className="desktop">
-                                                <Link to="/"><img alt="4-Forty Logo"
-                                                                  src="http://localhost:1337/uploads/4forty_logo_BW_horz_copy_300ccb0181.png"/></Link>
+                                                <Link to="/"><img alt="4-Forty Logo" src={desktopimageUrlImageUrl}/></Link>
                                             </li>
                                             <li className="mobile">
-                                                <Link to="/"><img alt="4-Forty Logo" src="http://localhost:1337/uploads/logo192_07a5f55591.png"/></Link>
+                                                <Link to="/"><img alt="4-Forty Logo" src={mobileImageUrl}/></Link>
                                             </li>
                                         </ul>
                                     </div>
