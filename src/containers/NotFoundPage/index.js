4ebbd7c2b3a7ca80ac3b3e484f7922d7eb702d66
ijadams <ijadams503@gmail.com>
2020-11-28 09:12:55 -0800

    not found css

--- src/containers/NotFoundPage/index.js
+++ src/containers/NotFoundPage/index.js
@@ -4,19 +4,21 @@
  * This is the component that will show when you have a 404
  */
 
-import React from 'react';
+import React from "react";
 
 function NotFoundPage(props) {
-    return (
-        <div>
-            <div className="logo--holder">
-                <a href="/"><img alt="logo" src="https://ijadams.s3.amazonaws.com/volta/volta-black-outline-orange.svg"/></a>
-            </div>
-            <div className="not-found">
-                <h1>The page you're looking for doesn't exist.</h1>
-            </div>
+  return (
+    <div>
+      <div className="not--found--container">
+        <div className="logo--holder">
+          <a href="/"><img alt="logo" src="https://ijadams.s3.amazonaws.com/volta/volta-black-outline-orange.svg"/></a>
         </div>
-    );
+        <div className="not-found">
+          <h1>The page you're looking for doesn't exist.</h1>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default NotFoundPage;
