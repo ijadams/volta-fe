9ef56426e34e20dd9f3bc53a6f186a6d568b043f
ijadams <ijadams503@gmail.com>
2020-11-30 14:14:11 -0800

    colabs:

--- src/components/HomeSections/HomeSectionThree/index.js
+++ src/components/HomeSections/HomeSectionThree/index.js
@@ -1,84 +1,38 @@
 /* eslint-disable */
 
 import React from "react";
-import TargetComponent from '../../TargetComponent';
+import TargetComponent from "../../TargetComponent";
 
 const HomeSectionThree = () => {
   return (
     <div>
-        <div id="home--section--three" className="home--section">
+      <div id="home--section--three" className="home--section">
 
+        <TargetComponent>
+          <div className="text--container">
+            <h1>Our Collaborators</h1>
+          </div>
+        </TargetComponent>
+
+        <div className="grid-container">
+          <div className="item--container">
             <TargetComponent>
-                <div className="text--container">
-                    <h1>Our Collaborators</h1>
-                </div>
+              <img className="grid-img"
+                   src="https://res.cloudinary.com/drnfburmr/image/upload/v1606505392/medium_Crooked_Coast_b3616fc863.jpg"
+                   alt="band image a"/>
+              <p>Crooked Coast</p>
             </TargetComponent>
-            
-            <div className="grid-container">
-                <div className="item--container">
-                    <TargetComponent>
-                        <img className="grid-img" src="https://bigpicturemediaonline.com/wp-content/uploads/2020/08/MCS-Peter-Yang-600x600.jpg" alt="band image 2"/>
-                        <p>Motion City Soundtrack</p>
-                    </TargetComponent>
-                </div>
-                
-                <div className="item--container">
-                    <TargetComponent>
-                        <img className="grid-img" src="https://bigpicturemediaonline.com/wp-content/uploads/2020/10/Screen-Shot-2020-10-09-at-2.57.35-PM.png" alt="band image a"/>
-                        <p>M.A.G.S.</p>
-                    </TargetComponent>
-                </div>
-
-                <div className="item--container">
-                    <TargetComponent>
-                        <img className="grid-img" src="https://bigpicturemediaonline.com/wp-content/uploads/2017/12/Bayside-Promo-Photo-2-600x600.jpg" alt="band image sa"/>
-                        <p>Bayside</p>
-                    </TargetComponent>
-                </div>
-
-                <div className="item--container">
-                    <TargetComponent>
-                        <img className="grid-img" src="https://bigpicturemediaonline.com/wp-content/uploads/2020/05/image-7-1-1.jpg" alt="band image sa"/>
-                        <p>Bayside</p>
-                    </TargetComponent>
-                </div>
-
-                <div className="item--container">
-                    <TargetComponent>
-                        <img className="grid-img" src="https://bigpicturemediaonline.com/wp-content/uploads/2020/08/unnamed-600x600.jpg" alt="band image s f"/>
-                        <p>Lauren Sanderson</p>
-                    </TargetComponent>
-                </div>
-
-                <div className="item--container">
-                    <TargetComponent>
-                        <img className="grid-img" src="https://bigpicturemediaonline.com/wp-content/uploads/2020/02/AO188529-1-copy-600x600.jpg" alt="band imagef "/>
-                        <p>SUM 41</p>
-                    </TargetComponent>
-                </div>
-
-                <div className="item--container">
-                    <TargetComponent>
-                        <img className="grid-img" src="https://bigpicturemediaonline.com/wp-content/uploads/2020/11/MOD-SUN-Press-Photo-@thatsnathanjames-600x600.jpeg" alt="basdand image"/>
-                        <p>MOD SUN</p>
-                    </TargetComponent>
-                </div>
-
-                <div className="item--container">
-                    <TargetComponent>
-                        <img className="grid-img" src="https://bigpicturemediaonline.com/wp-content/uploads/2020/06/Run-River-North-Credit-Serro-Park-600x600.jpg" alt="banasdd image"/>
-                        <p>Run River North</p>
-                    </TargetComponent>
-                </div>
-
-                <div className="item--container">
-                    <TargetComponent>
-                        <img className="grid-img" src="https://bigpicturemediaonline.com/wp-content/uploads/2020/02/E81B0274-600x600.jpg" alt="band imaasdge"/>
-                        <p>Smokeasac</p>
-                    </TargetComponent>
-                </div>
-             </div>
-        </div>   
+          </div>
+          <div className="item--container">
+            <TargetComponent>
+              <img className="grid-img"
+                   src="https://res.cloudinary.com/drnfburmr/image/upload/v1606774572/test12312_t4k7ch.jpg"
+                   alt="band image 2"/>
+              <p>The Quins</p>
+            </TargetComponent>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
